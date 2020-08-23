c8dde2abd060786dbea0106321a2c6e0a5478691
Adebayo Mohammed Olusiji <adebayomuhammad47@gmail.com>
2020-08-23 12:49:52 -0700

    feat: Setting up routes and firestorm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Product from "./Pages/Product";
+import Homepage from "./Pages/Homepage";
+import Cart from "./Pages/Cart";
+import Checkout from "./Pages/Checkout";
+import PrivateRoute from "./PrivateRoute";
+import { AuthProvider } from "./Auth";
+import "./Style.css";
+
+const DefaultLayout = ({ children, ...rest }) => {
+  return (
+    <div className="page page-dashboard">
+      <Header />
+
+      <div className="main">{children}</div>
+
+      <Footer />
+    </div>
+  );
+};
+
+const DefaultLayoutRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(matchProps) => (
+        <div className="DefaultLayout">
+          <DefaultLayout>
+            <Component {...matchProps} />
+          </DefaultLayout>
+        </div>
+      )}
+    />
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <DefaultLayoutRoute exact path="/" component={Homepage} />
+            <DefaultLayoutRoute exact path="/product" component={Product} />
+            <DefaultLayoutRoute exact path="/cart" component={Cart} />
+            <DefaultLayoutRoute exact path="/login" component={LoginPage} />
+            <DefaultLayoutRoute exact path="/signup" component={SignUpPage} />
+            <PrivateRoute exact path="/checkout" component={Checkout} />
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Style.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "./fonts/ProximaNovaA-Bold.woff";
+import "./fonts/ProximaNovaA-Extrabld.woff";
+import "./fonts/ProximaNovaA-Light.woff";
+import "./fonts/ProximaNovaA-Semibold.woff";
+import "./fonts/ProximaNovaA-Bold.woff";
+import "./fonts/ProximaNovaS-Regular.woff";
+import "./fonts/CRC35.woff";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
